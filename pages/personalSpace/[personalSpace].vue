0f5201a63d1fcfcbc6760cc11eba38b08a8594a1
Siddhartha Basu <basu.net@gmail.com>
2025-12-29 07:13:44 +0530

    formatted code

--- pages/personalSpace/[personalSpace].vue
+++ pages/personalSpace/[personalSpace].vue
@@ -44,7 +44,7 @@ const getContentPath = (routePath: string): string => {
 const { data: articles, error } = await useAsyncData(`blog-post-${path}`, async () => {
   // Convert path to content path for querying
   const contentPath = getContentPath(path)
-  
+
   // Try the converted content path first (most likely to work)
   let result = await queryCollection('content').path(contentPath).first()
 
